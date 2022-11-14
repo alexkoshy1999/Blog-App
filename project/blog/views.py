3b3543cda3340d847fa1a06533475a19b3c74ced
Your Name01~C:/Users/Alex Koshy/AppData/Local/Programs/Python/Python311/python.exe <alexkoshy36@gmail.com>
2022-11-14 20:26:12 +0530

    first commit

---
+++ project/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+posts = [
+    {
+        'author': 'Alex',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Django',
+        'title': 'Blog Post2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+def home(request):
+    context={
+        'post':posts
+    }
+    return render(request, 'blog/home.html',context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+   
