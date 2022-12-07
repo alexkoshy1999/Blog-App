df7a8c6ac367c1c907b572de4a02c36671fe30c3
Your Name01~C:/Users/Alex Koshy/AppData/Local/Programs/Python/Python311/python.exe <alexkoshy36@gmail.com>
2022-12-07 11:43:56 +0530

    Login

--- project/users/views.py
+++ project/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 '''from django.contrib.auth.forms import UserCreationForm'''
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -10,8 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request,f'Account has been created! You are now able to login')
+            return redirect('login')
     else:
         form = UserRegisterForm() 
     return render(request, 'users/register.html',{'form': form,'title':'Register'})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
